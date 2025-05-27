b1b9f10b24e194b013d08f200871c0f4de1d1ac0
Khodor-Jarkas <ikhodorjarkas@gmail.com>
2025-05-28 00:16:44 +0300

    testing

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ if (basename($_SERVER['PHP_SELF']) == 'index.php') {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>My Portfolio</title>
+    <title>My Portfoio</title>
     
     <!-- CSS -->
     <link rel="stylesheet" href="css/main.css">
